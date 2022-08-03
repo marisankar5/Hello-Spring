5fbaaef7fbb2fe5599a3db2760776a922c9ef6be
Mahes <43314132+marisankar5@users.noreply.github.com>
2022-08-03 12:33:24 +0530

    Update MyWebApplication.java

--- src/main/java/com/mkyong/MyWebApplication.java
+++ src/main/java/com/mkyong/MyWebApplication.java
@@ -3,11 +3,23 @@ package com.mkyong;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class MyWebApplication {
+// @SpringBootApplication
+// public class MyWebApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MyWebApplication.class, args);
-    }
+//     public static void main(String[] args) {
+//         SpringApplication.run(MyWebApplication.class, args);
+//     }
+
+@SpringBootApplication
+public class MyWebApplication extends SpringBootServletInitializer {
+  public static void main(String[] args) {
+      SpringApplication.run(MyWebApplication.class, args);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+      return builder.sources(MyWebApplication.class);
+  }
+}
 
 }
